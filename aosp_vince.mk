a1351d9a3a04c8209ab4da265599beccba2edde1
nekoprjkt <maseadek1986@gmail.com>
2024-02-23 06:55:28 +0000

    vince: wellcome dolby
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -25,6 +25,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
+# Dolby
+$(call inherit-product, vendor/dolby/ds/dolby-product.mk)
+
 # Inherit some common Horizondroid stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 

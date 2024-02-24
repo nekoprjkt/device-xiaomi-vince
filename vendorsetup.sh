687ff0b56ff805e73ee930f132fc65a2c13c6f07
neko kernel <maseadek1986@gmail.com>
2024-02-24 12:35:56 +0000

    vince: add vendorsetup
    Signed-off-by: neko kernel <maseadek1986@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+git clone --depth=1 https://github.com/nekoprjkt/vendor-xiaomi-vince -b 13 vendor/xiaomi/vince
+git clone --depth=1 https://gitlab.com/nekoprjkt/kernel-xiaomi-vince -b 14 kernel/xiaomi/vince
+git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera -b 13 platform/external/Camera
+git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang -b 17 prebuilts/clang/host/linux-x86/clang-aosp

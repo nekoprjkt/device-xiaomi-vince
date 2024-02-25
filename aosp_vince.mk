6a48858f624557ceaa670ec00bd04981077ca731
nekoprjkt <maseadek1986@gmail.com>
2024-02-25 12:41:35 +0000

    vince: Go Horizon Official
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -25,27 +25,28 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common Spark-OS stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit some common Horizondroid stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Spark flags
+# Horizon flags
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
 TARGET_BOOT_ANIMATION_RES := 720
+HORIZON_MAINTAINER := AL
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_BUILD_VIMUSIC := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := spark_vince
+PRODUCT_NAME := aosp_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
+# Nekoprjkt
+BUILD_USERNAME := android-build
+BUILD_HOSTNAME := neko-server
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V11.0.2.0.OEGMIXM release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/vince/vince:8.1.0/OPM1.171019.019/V11.0.2.0.OEGMIXM:user/release-keys"

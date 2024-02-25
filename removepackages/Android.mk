c68c8607fde5a8dd78daaf54cbd6bd1f43b434ef
nekoprjkt <maseadek1986@gmail.com>
2024-02-25 12:45:08 +0000

    vince: Update remove packages at build time
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Camera2 Maps Drive PrebuiltGmail YouTube Videos
+LOCAL_OVERRIDES_PACKAGES := Aperture Camera2 Maps Drive YouTube Videos GoogleTTS AndroidAutoStubPrebuilt TurboPrebuilt WellbeingPrebuilt Velvet SafetyHubPrebuilt Photos DevicePersonalizationPrebuiltPixel2020 PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null

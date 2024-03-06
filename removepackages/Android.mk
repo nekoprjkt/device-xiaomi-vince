37c52a10f7af01c27c02589b8524bdaf2d421a94
nekoprjkt <maseadek1986@gmail.com>
2024-03-06 14:12:50 +0700

    vince: Remove some more packages
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- removepackages/Android.mk
+++ removepackages/Android.mk
@@ -4,8 +4,26 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := removepackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Aperture Camera2 Maps Drive PrebuiltGmail YouTube Videos
+LOCAL_OVERRIDES_PACKAGES := \
+	Aperture \
+	arcore \
+	AICorePrebuilt \
+	AiWallpapers \
+	AndroidAutoStubPrebuilt \
+	Camera2 \
+	GoogleTTS \
+	Maps \
+	Photos \
+	PixelThemesStub \
+	PixelThemesStub2022_and_newer \
+	PixelWallpapers2023 \
+	PixelLiveWallpaperPrebuilt \
+	SafetyHubPrebuilt \
+	TurboPrebuilt \
+	Velvet \
+	WellbeingPrebuilt \
+	YouTube
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)

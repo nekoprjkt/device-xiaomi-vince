4d5ceaa5ca126469c9e1579ce7da1644f315adde
nekoprjkt <maseadek1986@gmail.com>
2024-03-13 08:24:07 +0000

    vince: swith to aidl light
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- device.mk
+++ device.mk
@@ -305,10 +305,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    android.hardware.light@2.0.vendor \
-    lights.msm8953
+    android.hardware.light-service.xiaomi_vince
 
 # Media
 PRODUCT_COPY_FILES += \

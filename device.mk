993999a29cb074c006a31af76ea7a559ef0f8a54
nekoprjkt <maseadek1986@gmail.com>
2024-03-15 04:29:54 +0000

    vince: audio: Import dolby atmos
    Signed-off-by: nekoprjkt <maseadek1986@gmail.com>

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
